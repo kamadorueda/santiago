05de647023a5ebad071afbed027f52ef216593b4
Kevin Amado <kamadorueda@gmail.com>
2022-03-19 00:32:29 -0500

    refactor: simplify example

--- examples/calculator.rs
+++ examples/calculator.rs
@@ -7,47 +7,55 @@ use santiago::lexer::lexer_rule::LexerRule;
 use santiago::parser::parse::parse;
 
 fn main() -> Result<(), String> {
-    // This is an example of an ambiguous grammar that sums numbers:
-    //   Sum  := Sum Plus Sum | "int"
-    //   Plus := "plus"
+    // Let's define an ambiguous grammar for adding integer numbers:
+    //
+    //   Sum = Sum Plus Sum
+    //       | "int"
+    //
+    //   Plus = "plus"
+    //
     let grammar: Vec<Rule> = GrammarBuilder::new()
         .map_to_rules("Sum", &["Sum", "Plus", "Sum"])
         .map_to_lexemes("Sum", &["int"])
         .map_to_lexemes("Plus", &["plus"])
         .finish();
 
-    // The lexer consumes the input string with the following rules:
+    // A lexer splits the input string into units
+    // of related characters called "Lexemes"
+    //
+    // For this calculator we are interested in the "+" operator
+    // and the digits of the integer numbers:
+    //
     //   "plus" := "+" (a character)
     //   "int"  := \d+ (regular expression for 1 or more digits)
     //     ∅    := " " (ignore whitespace)
-    let lexer_rules: Vec<LexerRule> = LexerBuilder::new()
+    //
+    let lexing_rules: Vec<LexerRule> = LexerBuilder::new()
         .string(&["initial"], "+", |matched, _| Some(("plus", matched)))
         .pattern(&["initial"], r"\d+", |matched, _| Some(("int", matched)))
         .string(&["initial"], " ", |_, _| None)
         .finish();
 
-    // First start by tokenizing the input
-    let lexemes: Vec<Lexeme> = lex(&lexer_rules, "1 + 2 + 3");
+    // Let's start by tokenizing the input
+    let lexemes: Vec<Lexeme> = lex(&lexing_rules, "11 + 22 + 33");
 
     // Now parse!
-    let forests = parse(&grammar, &lexemes)?;
+    let abstract_syntax_trees = parse(&grammar, &lexemes)?;
 
-    println!();
+    // And inspect the results that we just got
     println!("lexemes:");
     for lexeme in &lexemes {
         println!("  {lexeme:?}");
     }
 
-    println!();
     println!("Grammar:");
     for rule in &grammar {
         println!("  {rule}");
     }
 
-    println!();
     println!("Forest:");
-    for forest in forests {
-        println!("{forest}");
+    for ast in abstract_syntax_trees {
+        println!("{ast}");
     }
 
     Ok(())
