ec01b5f60c10003c4ce82d5d2aee0835c85918b0
Kevin Amado <kamadorueda@gmail.com>
2022-03-27 17:20:07 -0500

    feat: add binaries

---
+++ src/bin/nix.rs
@@ -0,0 +1,26 @@
+fn main() -> Result<(), String> {
+    use std::io::Read;
+
+    let lexing_rules = santiago::languages::nix::lexer_rules();
+    let grammar_rules = santiago::languages::nix::grammar();
+
+    let mut stdin = String::new();
+    std::io::stdin().read_to_string(&mut stdin).unwrap();
+
+    let lexemes = santiago::lexer::lex(&lexing_rules, &stdin).unwrap();
+
+    println!("Lexemes:");
+    for lexeme in &lexemes {
+        println!("  {lexeme}");
+    }
+
+    let abstract_syntax_trees =
+        santiago::parser::parse(&grammar_rules, &lexemes)?;
+
+    println!("AST:");
+    for ast in abstract_syntax_trees {
+        println!("{ast}");
+    }
+
+    Ok(())
+}
